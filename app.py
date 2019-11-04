7be3b17968a0b65e1dce7a2f75b9017fafcf458f
JoostdeJager <46704554+JoostdeJager@users.noreply.github.com>
2019-11-04 11:27:37 +0100

    database connection

--- app.py
+++ app.py
@@ -5,7 +5,10 @@ from flask import Flask
 from routes import *
 from excel_parser import parser
 
-print(parser.test())
+sheet = parser.get_sheet("db.xlsx")
+parser.add_record(sheet, ["Joost", 0, 0, 0, 0, "se"], "db.xlsx")
+records = parser.get_records("db.xlsx")
+print(parser.get_record(records, 0))
 
 app = Flask(__name__)
 app.register_blueprint(routes)
--- excel_parser/parser.py
+++ excel_parser/parser.py
@@ -1,2 +1,20 @@
-def test():
-	return "works"
+import pyexcel as pe
+
+# Functions regarding managing sheets
+
+def get_sheet(filename):
+    sheet = pe.get_sheet(file_name = "./db/" + filename)
+    return sheet
+	
+# Functions regarding records
+
+def get_records(filename):
+	records = pe.get_records(file_name = "./db/" + filename)
+	return records
+
+def add_record(sheet, record, filename):
+    sheet.row += record
+    sheet.save_as("./db/" + filename)
+
+def get_record(records, index):
+    return records[index]
