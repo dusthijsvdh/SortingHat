91431dc0f8eda4d63bd6cb05ab9710abf306fd97
JoostdeJager <46704554+JoostdeJager@users.noreply.github.com>
2019-11-05 13:26:10 +0100

    documentation

--- routes/db.py
+++ routes/db.py
@@ -1,25 +1,32 @@
-from . import routes
+# Predefined modules.
 from time import sleep
-from excel_parser import parser
 from flask import Flask, redirect, url_for
 
+# Our own modules.
+from . import routes
+from excel_parser import parser
+
+# This route returns all records in json format.
 @routes.route("/db/get_records")
 def get_records():
 	records = parser.get_records("db.xlsx")
 	return {"records": records}
 
+# This function returns one record as json.
 @routes.route("/db/get_record/<index>")
 def get_record(index):
 	records = parser.get_records("db.xlsx")
 	return records[int(index)]
 
+# This function adds a record with a specified name and then shows the record.
 @routes.route("/db/add_record/<name>")
 def add_record(name):
 	sheet = parser.get_sheet("db.xlsx")
-	parser.add_record(sheet, "db.xlsx", [name])
+	parser.add_record(sheet, "db.xlsx", [name,0,0,0,0,0])
 	sleep(0.2)
 	return redirect(url_for("routes.get_record", index = -1))
 
+# This function updates a record and then shows the changed record.
 @routes.route("/db/update_record/<int:index>/<field>/<value>")
 def update_record(index, field, value):
 	if field != "naam": value = int(value)
@@ -30,4 +37,5 @@ def update_record(index, field, value):
 	new_record = parser.record_to_list(record)
 
 	parser.update_record(sheet, "db.xlsx", index + 1, new_record)
-	return "ok"
+	
+	redirect(url_for("routes.get_record", index = index))
--- routes/vraag.py
+++ routes/vraag.py
@@ -7,41 +7,61 @@ from excel_parser import parser
 from . import routes
 from routes import db
 
+# This route lands you on the home page. Here you can fill in your name and then the server will redirect to the questions page.
 @routes.route("/", methods = ["GET", "POST"])
 def home():
+	# Check if we got form data.
 	if request.method == "POST" and request.form["name"] != "":
+		# Check for the easter egg's.
 		if request.form["name"].lower() == "harry potter":
 			return redirect(url_for("routes.uitslag", index = 0, uitslag = "Gryffindor!"))
 		elif request.form["name"].lower() == "jesse" or request.form["name"].lower() == "ryan":
 			return redirect(url_for("routes.uitslag", index = 0, uitslag = "SSSAAAAANG!!!!!"))
+		# If none of the easter-eggs is triggered, the code below will be called.
 		else:
+			# Get the name variable and add it to the database
 			name = request.form["name"]
 			db.add_record([name, 0,0,0,0,0])
+			# Get the index of the record in the db.
 			index = len(parser.get_records("db.xlsx")) - 1
+			# Redirect to the first question with the index of the record in the db.
 			return redirect(url_for("routes.vraag", vraagGetal = 1, index = index))
 	else:
+		# If we havent got any form data we will render the home page.
 		return render_template("index.html")
 
+# This route renders the questions
 @routes.route("/vraag/<int:vraagGetal>/<int:index>", methods = ["GET", "POST"])
 def vraag(vraagGetal, index):
+	# First we open the file where all of the questions are stored as json
 	with open("./routes/vragen.json") as f:
 		vragenDict = json.load(f)
 	
+	# Now we define the questions and the anwsers variables using the question number.
 	vraag = vragenDict["vragen"][vraagGetal - 1]
 	antwoorden = vragenDict["antwoorden"][vraagGetal - 1]
 
+	# We check to see if there is form data.
 	if request.method == "POST":
+		# If there is form data we first define a variable with the value, the awnser that the user gave.
 		form = request.form["antwoord"]
+		# Now we run the function handle_antwoorden from the exel_parser module, this function returns a 1 if a good awnser is given and stored,
 		if parser.handle_antwoorden(antwoorden, index, form) == 1:
+			# We check if this whas the last question.
 			if vraagGetal != len(vragenDict["vragen"]):
+				# If it whas not the last question we render the next question.
 				return redirect(url_for("routes.vraag", vraagGetal = vraagGetal + 1, index = index))
 			else:
+				# If it whas the last question we calculate the result and then we render the result page.
 				return redirect(url_for("routes.uitslag", index = index, uitslag = "SE natuurlijk"))
 		else:
-			return render_template("vraag.html", vraag = vraag, antwoord1 = antwoorden[0][0], antwoord2 = antwoorden[1][0], antwoord3 = antwoorden[2][0], antwoord4 = antwoorden[3][0])
+			# If the form returned an error we rerender the question.
+			return render_template("vraag4.html", vraag = vraag, antwoord1 = antwoorden[0][0], antwoord2 = antwoorden[1][0], antwoord3 = antwoorden[2][0], antwoord4 = antwoorden[3][0])
 	else: 	
-		return render_template("vraag.html", vraag = vraag, antwoord1 = antwoorden[0][0], antwoord2 = antwoorden[1][0], antwoord3 = antwoorden[2][0], antwoord4 = antwoorden[3][0])
+		# Is there is no form data we render the question.
+		return render_template("vraag4.html", vraag = vraag, antwoord1 = antwoorden[0][0], antwoord2 = antwoorden[1][0], antwoord3 = antwoorden[2][0], antwoord4 = antwoorden[3][0])
 
+# This route renders the result page
 @routes.route("/uitslag/<index>/<uitslag>")
 def uitslag(index, uitslag):
 	return render_template("uitslag.html", uitslag = uitslag)
